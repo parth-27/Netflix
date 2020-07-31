4e0ae799fe18f727053e67429bf50aaa7c99c0da
parth-27 <parth1020patel@gmail.com>
2020-07-31 10:43:32 +0530

    Categories and Poster added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,13 @@ function App() {
   return (
     <div className="App">
       <Row title="NETFLIX ORIGNALS" fetchUrl={requests.fetchNetflixOriginals}/>
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Trending Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,41 +1,51 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import axios from './axios';    // default import from our file axios.
+import axios from "./axios"; // default import from our file axios.
 
+import './Row.css';
 
-
-function Row({ title,fetchUrl }) {
-    
-    // using react hooks.
-    const [movies, setMovies] = useState([]);
-
-    // pulling the information from api when the row loads.
-    useEffect(() => {
-        // if [](empty array) run once when the row loads and don't run it again.
-        async function fetchData() {
-            // the value of request will be https://api.themoviedb.org/3/fetchUrl
-            const request = await axios.get(fetchUrl);
-            console.log(request);
-            setMovies(request.data.results);
-            return request;
-        }
-        fetchData();
-
-        // when we use any variable in async which is outside of the block we have to include it in the brackets.
-    }, [fetchUrl]);
-    
-
-    console.log(movies);
+const baseUrl = "https://image.tmdb.org/t/p/original/";
 
 
-    return (
-        <div>
-            {/* title */}
-            <h2>{title}</h2>
+function Row({ title, fetchUrl }) {
+  // using react hooks.
+  const [movies, setMovies] = useState([]);
 
-            {/* container -> posters */}
-        </div>
-    )
+  // pulling the information from api when the row loads.
+  useEffect(() => {
+    // if [](empty array) run once when the row loads and don't run it again.
+    async function fetchData() {
+      // the value of request will be https://api.themoviedb.org/3/fetchUrl
+      const request = await axios.get(fetchUrl);
+      console.log(request);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+
+    // when we use any variable in async which is outside of the block we have to include it in the brackets.
+  }, [fetchUrl]);
+
+  return (
+    <div className="row">
+      {/* title */}
+      <h2>{title}</h2>
+
+      <div className="row__posters">
+        {/* Poster */}
+        {movies.map((movie) => (
+			<img
+				key={movie.id}
+            	className="row__poster"
+            	src={`${baseUrl}${movie.poster_path}`}
+            	alt={movie.name}
+          />
+        ))}
+      </div>
+
+      {/* container -> posters */}
+    </div>
+  );
 }
 
-export default Row
+export default Row;
