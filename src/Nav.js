067dd039a3b886a356100a474532f31f44f8a1d5
parth-27 <parth1020patel@gmail.com>
2020-07-31 13:46:58 +0530

    nav bar added

--- src/Nav.js
+++ src/Nav.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react';
+import './Nav.css';
 
 function Nav() {
+
+    const [show, handleShow] = useState(false);
+
+    // scrolling and displaying the nav bar.
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100) {
+                handleShow(true);
+            }
+            else {
+                handleShow(false);
+            }
+        });
+        return () => {
+            window.removeEventListener("scroll");
+        };
+    }, []);
+
     return (
-        <div className="nav">
+        <div className={`nav ${show && "nav__black"}`}>
             <img className="nav__logo"
                 src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Logo_Netflix.png"
                 alt="Netflix Logo" />
-            <img class="nav__avatar" src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png" alt="Netflix Logo"></img>
+            <img className="nav__avatar" src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png" alt="Netflix Logo"></img>
         </div>
     )
 }
